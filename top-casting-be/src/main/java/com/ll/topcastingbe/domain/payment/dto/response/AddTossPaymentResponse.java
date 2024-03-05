073f20490ac75cd6afb4a5e0f48fe9841f78ca7e
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-03-05 21:29:47 +0900

    fix: 줄 구분 기호 수정

--- top-casting-be/src/main/java/com/ll/topcastingbe/domain/payment/dto/response/AddTossPaymentResponse.java
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/payment/dto/response/AddTossPaymentResponse.java
@@ -1,1 +1,22 @@
-package com.ll.topcastingbe.domain.payment.dto.response;import com.ll.topcastingbe.domain.payment.entity.Payment;import java.util.UUID;import lombok.Builder;@Builderpublic record AddTossPaymentResponse(UUID orderId,                                     Long price,                                     String customerAddress) {    public static AddTossPaymentResponse of(final Payment payment) {        AddTossPaymentResponse addTossPaymentResponse = AddTossPaymentResponse.builder()                .orderId(payment.getOrderId())                .price(payment.getPrice())                .customerAddress(payment.getCustomerAddress())                .build();        return addTossPaymentResponse;    }}
+package com.ll.topcastingbe.domain.payment.dto.response;
+
+import com.ll.topcastingbe.domain.payment.entity.Payment;
+import java.util.UUID;
+import lombok.Builder;
+
+
+@Builder
+public record AddTossPaymentResponse(UUID orderId,
+                                     Long price,
+                                     String customerAddress) {
+
+    public static AddTossPaymentResponse of(final Payment payment) {
+        AddTossPaymentResponse addTossPaymentResponse = AddTossPaymentResponse.builder()
+                .orderId(payment.getOrderId())
+                .price(payment.getPrice())
+                .customerAddress(payment.getCustomerAddress())
+                .build();
+
+        return addTossPaymentResponse;
+    }
+}
