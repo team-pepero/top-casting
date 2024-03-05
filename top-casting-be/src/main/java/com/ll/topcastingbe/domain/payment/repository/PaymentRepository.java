ecfa266a5fa9426a4cff564f8c9c93d670fff19e
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-03-05 19:27:07 +0900

    feat: paymentRepository 추가

---
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/payment/repository/PaymentRepository.java
@@ -0,0 +1,1 @@
+package com.ll.topcastingbe.domain.payment.repository;import com.ll.topcastingbe.domain.payment.entity.Payment;import org.springframework.data.jpa.repository.JpaRepository;public interface PaymentRepository extends JpaRepository<Payment, Long> {}
