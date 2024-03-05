c54bdbc3aa21c93dfa53b42408c0176267d18e80
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-03-05 17:59:07 +0900

    feat: MemberService

---
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/member/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.ll.topcastingbe.domain.member.service;
+
+import com.ll.topcastingbe.domain.member.dto.JoinRequestDto;
+import com.ll.topcastingbe.domain.member.entity.Member;
+import com.ll.topcastingbe.domain.member.repository.MemberRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public void join(JoinRequestDto joinRequestDto) {
+        Member member = Member.builder()
+                .username(joinRequestDto.getUsername())
+                .password(passwordEncoder.encode(joinRequestDto.getPassword()))
+                .nickname(joinRequestDto.getNickname())
+                .name(joinRequestDto.getName())
+                .email(joinRequestDto.getEmail())
+                .address(joinRequestDto.getAddress())
+                .birthDate(joinRequestDto.getBirthDate())
+                .phoneNumber(joinRequestDto.getPhoneNumber())
+                .build();
+
+        memberRepository.save(member);
+    }
+
+    public boolean checkNickname(String nickname) {
+        Optional<Member> om =  memberRepository.findByNickname(nickname);
+        if(om.isEmpty()) return false;
+        return true;
+    }
+}
