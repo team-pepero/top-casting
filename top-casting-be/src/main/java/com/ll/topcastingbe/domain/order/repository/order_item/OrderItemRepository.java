4e0025dc0c3f80b2469b81e461ea9aca6c92d2ab
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-03-05 21:40:03 +0900

    fix: 줄 구분 기호 수정

--- top-casting-be/src/main/java/com/ll/topcastingbe/domain/order/repository/order_item/OrderItemRepository.java
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/order/repository/order_item/OrderItemRepository.java
@@ -1,1 +1,7 @@
-package com.ll.topcastingbe.domain.order.repository.order_item;import com.ll.topcastingbe.domain.order.entity.OrderItem;import org.springframework.data.jpa.repository.JpaRepository;public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {}
+package com.ll.topcastingbe.domain.order.repository.order_item;
+
+import com.ll.topcastingbe.domain.order.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
