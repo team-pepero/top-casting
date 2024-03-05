c8aaa847f2e876e754e3a31eb109edf6a4c8f7c2
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-03-06 04:31:55 +0900

    feat: Security 상수 관리

---
+++ top-casting-be/src/main/java/com/ll/topcastingbe/global/security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.ll.topcastingbe.global.security;
+
+public final class SecurityConstants {
+
+    public static final String TOKEN_HEADER = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String TOKEN_TYPE = "JWT";
+
+}
