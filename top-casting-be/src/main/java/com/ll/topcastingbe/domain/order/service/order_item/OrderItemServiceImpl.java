a13c8dc39dff5acffc8b80a57b5fa8a632b3824c
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-03-05 23:30:33 +0900

    feat: OrderItem추가 기능 추가

--- top-casting-be/src/main/java/com/ll/topcastingbe/domain/order/service/order_item/OrderItemServiceImpl.java
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/order/service/order_item/OrderItemServiceImpl.java
@@ -1,10 +1,16 @@
 package com.ll.topcastingbe.domain.order.service.order_item;
 
 import com.ll.topcastingbe.domain.member.entity.Member;
+import com.ll.topcastingbe.domain.option.entity.Option;
+import com.ll.topcastingbe.domain.option.repository.OptionRepository;
 import com.ll.topcastingbe.domain.order.dto.order_item.request.AddOrderItemRequest;
 import com.ll.topcastingbe.domain.order.dto.order_item.request.ModifyOrderItemRequest;
 import com.ll.topcastingbe.domain.order.dto.order_item.response.FindOrderItemResponse;
+import com.ll.topcastingbe.domain.order.entity.OrderItem;
 import com.ll.topcastingbe.domain.order.entity.Orders;
+import com.ll.topcastingbe.domain.order.exception.EntityNotFoundException;
+import com.ll.topcastingbe.domain.order.exception.ErrorMessage;
+import com.ll.topcastingbe.domain.order.repository.order_item.OrderItemRepository;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +21,30 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class OrderItemServiceImpl implements OrderItemService {
+    private final OptionRepository optionRepository;
+    private final OrderItemRepository orderItemRepository;
 
     @Override
     @Transactional
     public void addOrderItem(Orders order, AddOrderItemRequest addOrderItemRequest) {
+        final Option option = optionRepository.findById(addOrderItemRequest.optionId())
+                .orElseThrow(() -> new EntityNotFoundException(ErrorMessage.ENTITY_NOT_FOUND));
 
+        final OrderItem orderItem = OrderItem.builder()
+                .order(order)
+                .option(option)
+                .itemQuantity(Long.valueOf(option.getStock()))
+                .totalPrice(getTotalPrice(option))
+                .build();
+
+        orderItemRepository.save(orderItem);
+    }
+
+    private Long getTotalPrice(final Option option) {
+        //todo 코드 더러움 수정 필요
+        final Long totalPrice = option.getItem().getItemPrice().longValue() * option.getStock();
+
+        return totalPrice;
     }
 
     @Override
