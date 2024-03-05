c35a8ea6c6d91b8cd48f95e4943d74b2b1511b16
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-03-05 19:59:47 +0900

    feat: OrderItemRepository 추가

---
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/order/repository/order_item/OrderItemRepository.java
@@ -0,0 +1,1 @@
+package com.ll.topcastingbe.domain.order.repository.order_item;import com.ll.topcastingbe.domain.order.entity.OrderItem;import org.springframework.data.jpa.repository.JpaRepository;public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {}
