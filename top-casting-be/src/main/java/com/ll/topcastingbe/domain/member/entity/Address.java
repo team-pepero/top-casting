e7695e3a1c91c5ba45674c7ffa3a0c98c1ac701b
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-03-06 00:02:01 +0900

    feat: 값타입 Address(address, zipcode) 추가

---
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/member/entity/Address.java
@@ -0,0 +1,17 @@
+package com.ll.topcastingbe.domain.member.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class Address {
+    private String address;
+    private String zipcode;
+}
