a0bda71f12b042ee1c5a708d6ce0231457213efc
nnWon <nwwn4627@gmail.com>
2024-03-05 13:51:09 +0900

    project init

---
+++ top-casting-be/src/main/java/com/ll/topcastingbe/TopCastingBeApplication.java
@@ -0,0 +1,13 @@
+package com.ll.topcastingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopCastingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopCastingBeApplication.class, args);
+	}
+
+}
---
+++ top-casting-be/src/test/java/com/ll/topcastingbe/TopCastingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.topcastingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopCastingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
