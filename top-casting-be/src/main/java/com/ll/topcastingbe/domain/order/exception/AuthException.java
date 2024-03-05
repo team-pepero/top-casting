226df01c73022979904b2901ccd8c19d4f069d65
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-03-05 16:01:19 +0900

    feat: 예외 처리 기능 추가

---
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/order/exception/AuthException.java
@@ -0,0 +1,1 @@
+package com.ll.topcastingbe.domain.order.exception;public class AuthException extends BusinessException {    public AuthException(ErrorMessage errorMessage) {        super(errorMessage);    }}
---
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/order/exception/BusinessException.java
@@ -0,0 +1,1 @@
+package com.ll.topcastingbe.domain.order.exception;public class BusinessException extends RuntimeException {    private final ErrorMessage errorMessage;    public BusinessException(final ErrorMessage errorMessage) {        super(errorMessage.getMessage());        this.errorMessage = errorMessage;    }}
