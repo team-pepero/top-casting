5385f63a183f76af542d3199d68c68830e65229f
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-03-06 22:45:12 +0900

    fix: 예외처리 반환값 통일화

--- top-casting-be/src/main/java/com/ll/topcastingbe/domain/order/exception/BusinessException.java
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/order/exception/BusinessException.java
@@ -1,10 +1,8 @@
 package com.ll.topcastingbe.domain.order.exception;
 
 public class BusinessException extends RuntimeException {
-    private final ErrorMessage errorMessage;
 
     public BusinessException(final ErrorMessage errorMessage) {
         super(errorMessage.getMessage());
-        this.errorMessage = errorMessage;
     }
 }
--- top-casting-be/src/main/java/com/ll/topcastingbe/domain/order/exception/ExceptionHandler.java
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/order/exception/ExceptionHandler.java
@@ -1,24 +1,44 @@
 package com.ll.topcastingbe.domain.order.exception;
 
-
-
+import com.ll.topcastingbe.global.dto.ErrorResponseDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 
 @Slf4j
 public class ExceptionHandler {
-    @org.springframework.web.bind.annotation.ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ExceptionResponseDto> businessException(BusinessException e) {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(AuthException.class)
+    public ResponseEntity<ErrorResponseDto> authException(final AuthException e) {
         log.error(e.getMessage(), e);
-        final ExceptionResponseDto exceptionResponseDto = ExceptionResponseDto.of(e.getMessage());
-        return ResponseEntity.badRequest().body(exceptionResponseDto);
+        final ErrorResponseDto errorResponseDto = ErrorResponseDto.builder()
+                .code(HttpStatus.FORBIDDEN.toString())
+                .message(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(errorResponseDto);
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponseDto> businessException(BusinessException e) {
+        log.error(e.getMessage(), e);
+        final ErrorResponseDto errorResponseDto = ErrorResponseDto.builder()
+                .code(HttpStatus.FORBIDDEN.toString())
+                .message(e.getMessage())
+                .build();
+        return ResponseEntity.badRequest().body(errorResponseDto);
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
-    public ResponseEntity<ExceptionResponseDto> exception(Exception e) {
+    public ResponseEntity<ErrorResponseDto> exception(Exception e) {
         log.error(e.getMessage(), e);
-        final ExceptionResponseDto exceptionResponseDto = ExceptionResponseDto.of(ErrorMessage.INTERNAL_SERVER_ERROR.getMessage());
-        return ResponseEntity.internalServerError().body(exceptionResponseDto);
+        final ErrorResponseDto errorResponseDto = ErrorResponseDto.builder()
+                .code(HttpStatus.FORBIDDEN.toString())
+                .message(e.getMessage())
+                .build();
+        return ResponseEntity.internalServerError().body(errorResponseDto);
     }
 
 }
