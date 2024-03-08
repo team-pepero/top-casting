1baf7af6a63a73fc7f5539ea8853e7b8e9c706e7
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-03-08 15:10:21 +0900

    feat : 키값을 받아오는 JwtProps

--- top-casting-be/src/main/java/com/ll/topcastingbe/global/security/JwtProps.java
+++ top-casting-be/src/main/java/com/ll/topcastingbe/global/security/JwtProps.java
@@ -1,13 +1,15 @@
 package com.ll.topcastingbe.global.security;
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
-@ConfigurationProperties("custom.jwt")
 public class JwtProps {
 
-    private String secretKey;
+    @Value("${custom.jwt.secretKey}")
+    public String secretKey;
+    @Value("${custom.jwt.refreshKey}")
+    public String refreshKey;
 }
