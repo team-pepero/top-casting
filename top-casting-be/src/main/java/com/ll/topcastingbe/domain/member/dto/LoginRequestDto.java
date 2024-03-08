13c07d37d672d7b656881060d22cb11d7e3ddd8b
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-03-08 15:19:20 +0900

    fix: LoginRequestDto

--- top-casting-be/src/main/java/com/ll/topcastingbe/domain/member/dto/LoginRequestDto.java
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/member/dto/LoginRequestDto.java
@@ -1,8 +1,12 @@
 package com.ll.topcastingbe.domain.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequestDto {
     private String username;
     private String password;
