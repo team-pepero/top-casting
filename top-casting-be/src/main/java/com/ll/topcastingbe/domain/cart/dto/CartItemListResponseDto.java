9b57157bc0f364a587c9cc4e7ab081f376dcdbdc
nnWon <nwwn4627@gmail.com>
2024-03-12 22:38:01 +0900

    refactor: CartItemListResponseDto 필드 수정

--- top-casting-be/src/main/java/com/ll/topcastingbe/domain/cart/dto/CartItemListResponseDto.java
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/cart/dto/CartItemListResponseDto.java
@@ -15,29 +15,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CartItemListResponseDto {
     private List<CartItemResponseDto> cartItems;
-    private BigDecimal totalPrice;
-    private BigDecimal deliveryFee;
-
-    public CartItemListResponseDto(List<CartItemResponseDto> cartItems, BigDecimal totalPrice) {
-        this.cartItems = cartItems;
-        this.totalPrice = totalPrice;
-        this.deliveryFee = ShippingConst.SHIPPING_FEE;
-    }
+    private BigDecimal shippingFee;
+    private BigDecimal freeShippingCond;
 
     public static CartItemListResponseDto toDto(List<CartItem> cartItems) {
-        List<CartItemResponseDto> cartItemResponseDtos = new ArrayList<>();
-        BigDecimal totalPrice = BigDecimal.ZERO;
-        for (CartItem cartItem : cartItems) {
-            cartItemResponseDtos.add(CartItemResponseDto.toDto(cartItem));
-            BigDecimal itemPrice = cartItem.getOption().getItem().getItemPrice();
-            totalPrice = totalPrice.add(itemPrice.multiply(BigDecimal.valueOf(cartItem.getItemQuantity())));
-        }
+        List<CartItemResponseDto> cartItemResponseDtos = cartItems.stream()
+                .map(CartItemResponseDto::toDto).toList();
 
-        //총 금액이 무료배송조건이상인 경우
-        if (ShippingConst.FREE_SHIPPING_COND.compareTo(totalPrice) <= 0) {
-            return new CartItemListResponseDto(cartItemResponseDtos, totalPrice, BigDecimal.ZERO);
-        }
-        //총 금액이 무료배송조건보다 작은 경우
-        return new CartItemListResponseDto(cartItemResponseDtos, totalPrice);
+        return new CartItemListResponseDto(cartItemResponseDtos, ShippingConst.SHIPPING_FEE, ShippingConst.FREE_SHIPPING_COND);
     }
 }
