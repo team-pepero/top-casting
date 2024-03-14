55828ad30b0b1df5153f7fdb5b0e3790f192adaa
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-03-14 15:49:56 +0900

    feat: Validation추가

--- top-casting-be/src/main/java/com/ll/topcastingbe/domain/order/dto/order_item/AddOrderItemDto.java
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/order/dto/order_item/AddOrderItemDto.java
@@ -1,11 +1,14 @@
 package com.ll.topcastingbe.domain.order.dto.order_item;
 
 import com.ll.topcastingbe.domain.order.dto.order_item.request.AddOrderItemRequest;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
-public record AddOrderItemDto(Long cartItemId,
-                              Long itemQuantity) {
+public record AddOrderItemDto(@NotNull(message = "cartItem의 값을 필수입니다.") Long cartItemId,
+                              @Min(value = 1, message = "상품의 갯수는 1이상이어야 합니다.")
+                              @NotNull(message = "상품의 갯수는 필수입니다.") Long itemQuantity) {
 
     public AddOrderItemRequest toOrderItemDto() {
         final AddOrderItemRequest addOrderItemRequest = AddOrderItemRequest.builder()
