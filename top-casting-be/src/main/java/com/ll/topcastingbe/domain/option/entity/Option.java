7f32305103bc5fce90727e7493f9245b41d59d7b
nnWon <nwwn4627@gmail.com>
2024-04-03 14:18:28 +0900

    add: Option 클래스에 @Table 애노테이션 추가

--- top-casting-be/src/main/java/com/ll/topcastingbe/domain/option/entity/Option.java
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/option/entity/Option.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "options")
 @Builder
 @Getter
 public class Option {
