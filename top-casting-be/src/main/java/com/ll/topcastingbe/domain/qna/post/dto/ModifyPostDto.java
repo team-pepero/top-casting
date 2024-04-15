17b0f8822e4d2b594f92424a99cd660eddda5df2
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2024-04-15 14:05:42 +0900

    feat: 공지사항 모두조회, 삭제, 수정

--- top-casting-be/src/main/java/com/ll/topcastingbe/domain/qna/post/dto/ModifyPostDto.java
+++ top-casting-be/src/main/java/com/ll/topcastingbe/domain/qna/post/dto/ModifyPostDto.java
@@ -4,7 +4,7 @@ import com.ll.topcastingbe.domain.qna.post.dto.request.ModifyPostRequest;
 
 public record ModifyPostDto(String title, String content) {
 
-    public ModifyPostRequest toModifyPostDto(String title, String content) {
+    public ModifyPostRequest toModifyPostRequest(final ModifyPostDto modifyPostDto) {
         final ModifyPostRequest modifyPostRequest = ModifyPostRequest.builder()
                 .title(title)
                 .content(content)
